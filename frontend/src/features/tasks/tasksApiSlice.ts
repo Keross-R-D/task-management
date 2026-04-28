5fa84566a161bc5718dc0efa2e0104a697f12845
SourangshuKundu723 <kundusourangshu@gmail.com>
2026-04-28 12:41:24 +0530

    Completed Dashboard section and added resource utilisation and project status report page UI

--- frontend/src/features/tasks/tasksApiSlice.ts
+++ frontend/src/features/tasks/tasksApiSlice.ts
@@ -93,6 +93,7 @@ export const tasksApiSlice = apiSlice.injectEndpoints({
 
 export const {
   useGetTasksByProjectQuery,
+  useLazyGetTasksByProjectQuery,
   useGetTasksBacklogQuery,
   useGetTasksBySprintQuery,
   useCreateTaskMutation,
